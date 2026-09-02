febac3bdfa1c8a84e0e64eaf1ed955e57f301daa
vaibhavrawat-error404 <johnsnow3112006@gmail.com>
2026-09-03 01:42:54 +0530

    used getline and universal library also the datatypes

--- basic.cpp
+++ basic.cpp
@@ -1,14 +1,15 @@
-// #include<iostream>
 
+//basic skeleton of c++ program
+// #include<iostream>
 // using namespace std;
 // int main(){
 //     int a=20;
-//     int b=23;
-//     cout<< a+b;
+//     cout<< a;
 
 //     return 0;
 // }
 
+//printing an output
 // #include<iostream>
 // int main()
 // {
@@ -16,14 +17,34 @@
 //     return 0;
 // }
 
+//using namespace std; and taking input
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y,z;
+//     cin >>x>>y>>z;
+//     cout <<x+y+z;
 
-#include<iostream>
-using namespace std;
-int main()
-{
-    int x,y,z;
-    cin >>x>>y>>z;
-    cout <<x+y+z;
+//     return 0;
+// }
 
-    return 0;
-}
+//taking string input and printing them
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     string s1,s2;
+//     cin>>s1>>s2;
+//     cout<<s1<<" "<<s2;
+//     return 0;
+// }
+
+//used bits/stdc++.h and getline() funtion
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     string s;
+//     getline(cin,s);
+//     cout<<s;
+// }
