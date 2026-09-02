f3f261b9043742466b4cc322750b6bfe284b5a96
vaibhavrawat-error404 <johnsnow3112006@gmail.com>
2026-09-02 23:14:01 +0530

    done a lot of things to setup c++ in vscode

---
+++ basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num > 5) {
+        cout << "hey\n";
+    }
+    else{
+        cout<<"hello\n";
+    }
+
+    return 0;
+}
