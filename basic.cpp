e9ca95eead857e83267e2532b269b249b14171cd
vaibhavrawat-error404 <johnsnow3112006@gmail.com>
2026-09-04 18:26:05 +0530

    switch case

--- basic.cpp
+++ basic.cpp
@@ -134,3 +134,41 @@
 //         cout<<"Inappropriate entery!!";
 //     }
 // }
+
+//switch case
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int day;
+    cout<<"Enter number(1-7) for each day of week:\n";
+    cin>>day;
+
+    switch(day){
+        case 1:
+            cout<<"MONDAY";
+            break;
+        case 2:
+            cout<<"TUESDAY";
+            break;
+        case 3:
+            cout<<"WEDNESDAY";
+            break;
+        case 4:
+            cout<<"THURSDAY";
+            break;
+        case 5:
+            cout<<"FRIDAY";
+            break;
+        case 6:
+            cout<<"SATURDAY";
+            break;
+        case 7:
+            cout<<"SUNDAY";
+            break;
+        default:
+            cout<<"Invalid Input!!";
+            break;
+        }
+}
