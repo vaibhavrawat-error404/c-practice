87736df8b48a091162548b6e987821989ad96932
vaibhavrawat-error404 <johnsnow3112006@gmail.com>
2026-09-05 13:28:16 +0530

    right angled triangle pattern

--- patterns/right_angle_triangle.cpp
+++ patterns/right_angle_triangle.cpp
@@ -1,12 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void pattern(int n){
-    int i;
-    int j;
-
+void print_pattern(int n){
+    int i,j;
     for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
+        for(j=0;j<=i;j++){
             cout<<"* ";
         }
         cout<<"\n";
@@ -17,8 +15,9 @@ int main(){
     int t;
     cin>>t;
     for(int i=0;i<t;i++){
-    int n;
-    cin>>n;
-    pattern(n);
+        int n;
+        cin>>n;
+        print_pattern(n);
     }
+    return 0;
 }
