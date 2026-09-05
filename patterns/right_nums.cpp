f7b7d27f187580dc395e2ac4ef2d60a927d5f3eb
vaibhavrawat-error404 <johnsnow3112006@gmail.com>
2026-09-05 13:43:44 +0530

    right angle triangle with numbers instead of asterisk

---
+++ patterns/right_nums.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_pattern(int n){
+    int i,j;
+    for(int i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        int n;
+        cin>>n;
+        print_pattern(n);
+    }
+    return 0;
+}
