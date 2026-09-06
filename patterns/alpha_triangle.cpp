bb31fda472de4b4883e6823140aab48e45513d7d
vaibhavrawat-error404 <johnsnow3112006@gmail.com>
2026-09-07 02:36:48 +0530

    triangle with alphabetical order

---
+++ patterns/alpha_triangle.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_pattern(int n){
+    int i,j,k;
+    int space=2*(n-1);
+    for(i=1;i<=n;i++){
+        for(char j='A';j<'A'+i;j++){
+            cout<<j;
+        }
+        cout<<"\n";
+
+    }  
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        int n;
+        cin>>n;
+        print_pattern(n);
+
+    }
+    return 0;
+}
