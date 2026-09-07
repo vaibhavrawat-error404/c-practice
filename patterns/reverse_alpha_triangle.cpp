5ca38499bad7a5b929aa6961cde178ac7e22c9a9
vaibhavrawat-error404 <johnsnow3112006@gmail.com>
2026-09-07 22:29:42 +0530

    method 2 for solving reverse triangle

--- patterns/reverse_alpha_triangle.cpp
+++ patterns/reverse_alpha_triangle.cpp
@@ -1,11 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print_pattern(int n){
-    int i,j,k;
-    int space=2*(n-1);
+void print_pattern_m1(int n){
+    int i,j;
     for(i=n;i>=1;i--){
         for(char j='A';j<'A'+i;j++){
+            cout<<j;
+        }
+        cout<<"\n";
+
+    }  
+}
+void print_pattern_m2(int n){
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(char j='A';j<'A'+(n-i-1);j++){
             cout<<j;
         }
         cout<<"\n";
@@ -19,7 +28,7 @@ int main(){
     for(int i=1;i<=t;i++){
         int n;
         cin>>n;
-        print_pattern(n);
+        print_pattern_m1(n);
 
     }
     return 0;
